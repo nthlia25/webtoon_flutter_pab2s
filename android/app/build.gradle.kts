382ac6fa4807f4a1557513ec0b9386c6ddea4d9e
nthlia25 <nathaliamargaretha2506@gmail.com>
2026-06-21 22:55:20 +0700

    ..

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
@@ -46,7 +45,7 @@ flutter {
     source = "../.."
 }
 
-depedencies{
-    implementation(platform("com.google.firebase:firebase-bom:34.14.1"))
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
     implementation("com.google.firebase:firebase-analytics")
 }
